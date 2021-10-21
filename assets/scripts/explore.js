57c29f98fc8ec87800b7eec68643cf9cbd032167
Jerry Lu <jclu2gm@gmail.com>
2021-10-20 21:34:09 -0700

    Modified expose and explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,36 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+let voices;
+
 function init() {
-  // TODO
+  speechSynthesis.onvoiceschanged = populateVoices;
+
+  let button = document.querySelector('button');
+  let img = document.querySelector('img');
+  button.addEventListener('click', () => {
+    let utter = new SpeechSynthesisUtterance();
+    utter.text = document.getElementById('text-to-speak').value;
+    utter.voice = voices[document.getElementById('voice-select').selectedOptions[0].value];
+    utter.addEventListener('start', () => {
+      img.src = "assets/images/smiling-open.png";
+    });
+    utter.addEventListener('end', () => {
+      img.src = "assets/images/smiling.png";
+    });
+    speechSynthesis.speak(utter);
+  });
+  
+}
+
+function populateVoices() {
+  voices = speechSynthesis.getVoices();
+  for(let i = 0; i < voices.length; i++) {
+    let opt = document.createElement('option');
+    opt.value = i;
+    opt.textContent = voices[i].name + '(' + voices[i].lang + ')';
+    opt.setAttribute('data-name', voices[i].name);
+    opt.setAttribute('data-lang', voices[i].lang);
+    document.getElementById('voice-select').appendChild(opt);
+  }
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,38 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const hornSel = document.getElementById('horn-select');
+  const aud = document.querySelector('audio');
+  hornSel.addEventListener('change', () => {
+    const img = document.querySelector('img');
+    img.src = "assets/images/" + hornSel.value + ".svg";
+    aud.src = "assets/audio/" + hornSel.value + ".mp3";
+  });
+
+  const vol = document.getElementById('volume');
+  vol.addEventListener('input', () => {
+    const volImg = document.querySelectorAll('img')[1];
+    if(vol.value == 0) {
+      volImg.src = "assets/icons/volume-level-0.svg";
+    }
+    else if(vol.value < 33) {
+      volImg.src = "assets/icons/volume-level-1.svg";
+    }
+    else if(vol.value < 67) {
+      volImg.src = "assets/icons/volume-level-2.svg";
+    }
+    else {
+      volImg.src = "assets/icons/volume-level-3.svg";
+    }
+    aud.volume = (vol.value/100);
+  });
+
+  const button = document.querySelector('button');
+  const jsConfetti = new JSConfetti();
+  button.addEventListener('click', () => {
+    aud.play();
+    if(hornSel.value == "party-horn") {
+      jsConfetti.addConfetti();
+    }
+  });
 }
